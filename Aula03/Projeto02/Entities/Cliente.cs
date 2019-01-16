461f78b8db853b99a2df965bfdc167cae31764af
Luana <luanafernandes73@gmail.com>
2019-01-16 20:53:07 -0200

    Finalizando o Projeto02

---
+++ Aula03/Projeto02/Entities/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Entities
+{
+    public class Cliente
+    {
+        #region Atributos
+        private int idCliente;
+        private string nome;
+        private string email;
+        #endregion
+
+        #region Encapsulamento
+        public int IdCliente { get => idCliente; set => idCliente = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        #endregion
+
+        #region Construtores
+        //Construtor default
+        public Cliente()
+        {
+
+        }
+
+        //Sobrecarga (Overloading)
+        public Cliente(int idCliente, string nome, string email)
+        {
+            IdCliente = idCliente;
+            Nome = nome;
+            Email = email;
+        }       
+        #endregion
+    }
+}
--- Aula03/Projeto02/Program.cs
+++ Aula03/Projeto02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto02.Entities;
 
 namespace Projeto02
 {
@@ -10,6 +11,40 @@ namespace Projeto02
     {
         static void Main(string[] args)
         {
+            List<Cliente> lista = new List<Cliente>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Cliente cliente = new Cliente();
+
+                Console.WriteLine($"\nEntre com o {i + 1}º Cliente:\n");
+
+                Console.WriteLine("Id do Cliente.....: ");
+                cliente.IdCliente = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Nome do Cliente.....: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.WriteLine("Email do Cliente.....: ");
+                cliente.Email = Console.ReadLine();
+
+                lista.Add(cliente);
+            }
+
+            Console.WriteLine("\nClientes adicionados com sucesso!\n");
+            Console.WriteLine("Quantidade de Cliente: " + lista.Count);
+            Console.WriteLine("\n\n");
+
+            //Varrendo todos os elementos da lista
+            foreach(Cliente c in lista)
+            {
+                Console.WriteLine("\tID.......: " + c.IdCliente);
+                Console.WriteLine("\tNOME.....: " + c.Nome);
+                Console.WriteLine("\tEMAIL....: " + c.Email);
+                Console.WriteLine("\t----------------------");
+            }
+
+            Console.ReadKey();
         }
     }
 }
